ab88d02d5fab7dafcd0966f1f7c7fc2bc147d89b
Andrew Winters <andrew.ross.winters@liu.se>
2022-04-01 07:32:34 +0200

    add a 3D smoke test for a simple extrusion

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,13 +22,22 @@ isdir(outdir) && rm(outdir, recursive=true)
     @test generate_mesh(control_file, verbose=true) isa String
   end
 
-  @testset "generate_mesh() with ABAQUS output" begin
+  @testset "generate_mesh() in 2D with ABAQUS output" begin
     control_file = joinpath(HOHQMesh.examples_dir(), "IceCreamCone_Abaqus.control")
     generate_mesh(control_file)
     parse_mesh = abaqus_read_mesh(joinpath(outdir, "IceCreamCone_Abaqus.inp"))
     # set some reference values for comparison. These are the corner IDs for element 114
     reference_ids = [140, 141, 154, 153]
     @test parse_mesh["elements"][114] == reference_ids
+  end
+
+  @testset "generate_mesh() in 3D with ABAQUS output" begin
+    control_file = joinpath(HOHQMesh.examples_dir(), "HalfCircle3DRot.control")
+    generate_mesh(control_file)
+    parse_mesh = abaqus_read_mesh(joinpath(outdir, "HalfCircle3DRot.inp"))
+    # set some reference values for comparison. These are the corner IDs for element 246
+    reference_ids = [297, 321, 322, 302, 363, 387, 388, 368]
+    @test parse_mesh["elements"][246] == reference_ids
   end
 
   @testset "generate_mesh() with invalid format" begin
