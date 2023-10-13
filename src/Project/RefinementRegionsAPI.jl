574ead29bcef48fe1ac48946ba3363d5b07666a0
Daniel Doehring <doehringd2@gmail.com>
2023-10-13 11:26:30 +0200

    Update RefinementRegionsAPI.jl Docstring (#34)

--- src/Project/RefinementRegionsAPI.jl
+++ src/Project/RefinementRegionsAPI.jl
@@ -4,7 +4,7 @@
                         center, meshSize,
                         width)
 
-Create refinement center of `type` "smooth" or "sharp" centered at `center = [x,y,z]``
+Create refinement center of `type` "smooth" or "sharp" centered at `center = [x,y,z]`
 with a mesh size `meshSize` spread over a radius `width`.
 """
 function newRefinementCenter(name::String, type::String,
